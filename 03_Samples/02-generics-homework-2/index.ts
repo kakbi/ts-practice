ebbdfa2fdcbccc9e93e88d56c2cb40777a1c2737
Vadym Chebotar <dramvoda@gmail.com>
2026-02-04 23:59:25 +0200

    prectice with generic

---
+++ 03_Samples/02-generics-homework-2/index.ts
@@ -0,0 +1,55 @@
+type ID = string | number;
+
+interface Entity {
+    readonly id: ID;
+}
+
+class Repository<T extends Entity> {
+    protected items: Map<ID, T> = new Map();
+
+    add(item: T): void {
+        this.items.set(item.id, item);
+    }
+
+    getById(id: ID): T | undefined {
+        return this.items.get(id);
+    }
+
+    getAll(): readonly T[] {
+        return Array.from(this.items.values());
+    }
+
+    remove(id: ID): boolean {
+        return this.items.delete(id);
+    }
+}
+
+interface User extends Entity {
+    name: string;
+    email: string;
+}
+
+class UserRepository extends Repository<User> {
+    findByEmail(email: string): User | undefined {
+        return this.getAll().find((user) => user.email === email);
+    }
+}
+
+class UserService {
+    constructor(private readonly repo: UserRepository) {}
+
+    async register(user: User): Promise<User> {
+        const exists = this.repo.findByEmail(user.email);
+
+        if (exists) {
+            throw new Error('User already exists');
+        }
+
+        this.repo.add(user);
+        return user;
+    }
+
+    getUsers(): readonly User[] {
+        return this.repo.getAll();
+    }
+}
